af06ce6f62814d74064bc126ceae2b67f4369a03
larafdouglas <douglas.laraf@gmail.com>
2020-08-22 11:01:54 -0400

    navbar active

--- run.py
+++ run.py
@@ -2,8 +2,14 @@ from flask import Flask
 from flask import render_template
 app = Flask(__name__)
 
-@app.route("/teste")
+@app.route("/")
 def index():
     return render_template('index.html')
+
+@app.route("/habilidades")
+def habilidades():
+    return render_template('habilidades.html')
+
+
 if __name__=='__main__':
 	app.run()
