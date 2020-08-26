31cfbcde9a354ec7d69242bbbb5edb895154077a
larafdouglas <douglas.laraf@gmail.com>
2020-08-26 14:38:36 -0400

    pip flask wtf

--- run.py
+++ run.py
@@ -2,8 +2,9 @@ from meuapp import app
 from meuapp import manager
 from meuapp.models import tables
 from meuapp.models.tables import Login
+from meuapp.models.form import MyForm
 from meuapp import db
-from flask import render_template
+from flask import render_template, redirect
 
 
 @app.route("/")
@@ -14,10 +15,22 @@ def index():
 def habilidades():
     return render_template('habilidades.html')
 
-@app.route('/teste',methods=['GET','POST'])
-def teste():
-	usuarios=Login.query.all()
-	return render_template('teste.html',usuarios=usuarios)
+@app.route("/cadastrar_livro")
+def cadastrar_livro():
+    return render_template('cadastrar_livro.html')
+
+#@app.route('/teste',methods=['GET','POST'])
+#def teste():
+	
+	#return render_template('teste.html',usuarios=usuarios)
+
+@app.route('/teste', methods=['GET', 'POST'])
+def submit():
+    form = MyForm()
+    usuarios=Login.query.all()
+    if form.validate_on_submit():
+        return redirect('/')
+    return render_template('teste.html', usuarios=usuarios,form=form)
 
 
 #i=Login('dougladd2s','adfddads12345')
